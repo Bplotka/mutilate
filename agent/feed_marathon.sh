707c2d3c2296a2cd0cb399ebb74a6ff4140f5e87
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-01-14 18:53:01 +0100

    Fixed script.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- agent/feed_marathon.sh
+++ agent/feed_marathon.sh
@@ -2,5 +2,5 @@
 
 MARATHON=$1
 
-curl -X POST "http://${MARATHON}/v2/apps" -d ./mesos_marathon_app.json \
--H "Content-type: application/json"
+curl -X POST "${MARATHON}/v2/apps" -d @"./mesos_marathon_app.json" \
+ -H "Content-type: application/json"
